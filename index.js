923bdba7771796252ad9e9e86536660e195ee79e
Emily Sousa <contemilyaraujo@gmail.com>
2023-11-13 23:37:34 -0300

    Rodapé, finalmente galeria e hovers

--- index.js
+++ index.js
@@ -1,4 +1,145 @@
+
+// Navbar Scroll
+
 window.addEventListener("scroll", function(){
   let nav = document.querySelector('#menu')
-  nav.classList.toggle('rolagem',window.scrollY > 0)
+  nav.classList.toggle('rolagem',window.scrollY > 150) 
 })
+
+
+// Mudança de logo conforme Scroll
+jQuery("document").ready(function($){
+	    	
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$("#logo").attr("src", "img/incept-ligth.png");
+		} else {
+			$("#logo").attr("src", "img/incept-ligth.png");
+		}      
+	});
+
+});
+
+
+
+// Animações ao Scroll
+
+const my0bserver = new IntersectionObserver((entries) => {
+  entries.forEach( (entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible')
+    } else {
+      entry.target.classList.remove('visible')
+    }
+  })
+})
+
+const elements = document.querySelectorAll('.ocult')
+
+elements.forEach( (element) => my0bserver.observe(element))
+
+
+
+(function ($) {
+  "use strict";
+  
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+      function toggleNavbarMethod() {
+          if ($(window).width() > 992) {
+              $('.navbar .dropdown').on('mouseover', function () {
+                  $('.dropdown-toggle', this).trigger('click');
+              }).on('mouseout', function () {
+                  $('.dropdown-toggle', this).trigger('click').blur();
+              });
+          } else {
+              $('.navbar .dropdown').off('mouseover').off('mouseout');
+          }
+      }
+      toggleNavbarMethod();
+      $(window).resize(toggleNavbarMethod);
+  });
+  
+  
+  // Back to top button
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+  });
+  $('.back-to-top').click(function () {
+      $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+      return false;
+  });
+
+
+  // Portfolio isotope and filter
+  var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+  });
+  $('#portfolio-flters li').on('click', function () {
+      $("#portfolio-flters li").removeClass('active');
+      $(this).addClass('active');
+
+      portfolioIsotope.isotope({filter: $(this).data('filter')});
+  });
+
+
+  // Team carousel
+  $(".team-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1000,
+      margin: 30,
+      dots: false,
+      loop: true,
+      nav : true,
+      navText : [
+          '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+          '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+      ],
+      responsive: {
+          0:{
+              items:1
+          },
+          576:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          992:{
+              items:3
+          }
+      }
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1500,
+      margin: 30,
+      dots: true,
+      loop: true,
+      center: true,
+      responsive: {
+          0:{
+              items:1
+          },
+          576:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          992:{
+              items:3
+          }
+      }
+  });
+  
+})(jQuery);
+
